ce9cbc3a06eecc59594acd5b7eb1dacfb8dc2ea5
vijayr96 <vijay.workmail1@gmail.com>
2020-07-04 20:04:41 +0530

    adding the golang basic learning files

---
+++ Basics/hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello world")
+
+	foo()
+
+	for i := 0; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("Im now in foo")
+}
