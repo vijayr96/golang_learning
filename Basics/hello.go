deaecfb091be7bfdacba9dd82cd904c9498b9f52
vijayr96 <vijay.workmail1@gmail.com>
2020-07-04 20:38:03 +0530

    minor changes in code

--- Basics/hello.go
+++ Basics/hello.go
@@ -11,11 +11,11 @@ func main() {
 
 	for i := 0; i <= 100; i++ {
 		if i%2 == 0 {
-			fmt.Println(i)
+			fmt.Println(i, "Printing only even numbers")
 		}
 	}
 }
 
 func foo() {
-	fmt.Println("Im now in foo")
+	fmt.Println("Im now in the function foo and calling it from outside of main")
 }
