5c9d6f48ac3111780604d22baaf66c100e6e299c
vijayr96 <vijay.workmail1@gmail.com>
2020-07-04 21:23:35 +0530

    added another program in basics

---
+++ Basics/gopher_identifier.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//Declare a variable and assigning value to it directly using a gopher ' := '
+
+	x := 40
+	fmt.Println(x)
+	y := "This is a short declaration operator := "
+	fmt.Println(y)
+}
